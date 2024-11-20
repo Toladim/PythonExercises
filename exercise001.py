56aef4e34ecfb27383c02a266a3b27c1d6f34932
Marcin <marcingieras1a@gmail.com>
2024-11-20 16:31:06 +0100

    Hello World

---
+++ exercise001.py
@@ -0,0 +1,5 @@
+name = str(input("Give me your name! "))
+age = int(input("Give me your age"))
+current_year = 2024
+year = current_year + 100 - age
+print(name, f" you will be 100 years old in {year}")
