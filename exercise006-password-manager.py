873edf80a1b8e0caff089ca2e57e80f59a6f6d1b
Marcin <marcingieras1a@gmail.com>
2024-11-21 20:32:39 +0100

    WIP: exc06

---
+++ exercise006-password-manager.py
@@ -0,0 +1,13 @@
+pwd = input("What is your master password? ")
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view,add)? Press 'q' to quit").lower()
+    
+    if mode == "q":
+        break
+    elif mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid mode.")
+        continue
