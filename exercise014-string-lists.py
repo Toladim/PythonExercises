a6dca9a1d5b8e299542cf387cf9f7d6a6ea55dc2
Marcin <marcingieras1a@gmail.com>
2024-11-25 17:53:26 +0100

    exc015

--- exercise014-string-lists.py
+++ exercise014-string-lists.py
@@ -1,7 +1,7 @@
 """ Ask the user for a string and print out whether this string is a palindrome or not.
  (A palindrome is a string that reads the same forwards and backwards.)"""
 
-user_string = input("Type some string to prove that is palindrome or not: ")
+user_string = input("Type some string to check if it is a palindrome: ")
 
 def is_palindrome(string):
     normalized_string = "".join(string.lower().strip())
