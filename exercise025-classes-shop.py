f97bd217ac85a93263647f3e6a7e5600ad5782f3
Marcin <marcingieras1a@gmail.com>
2024-12-07 22:08:17 +0100

    exc025.1

--- exercise025-classes-shop.py
+++ exercise025-classes-shop.py
@@ -6,12 +6,8 @@ class Product:
     
 class OrderItem:
     def __init__(self, product: Product, quantity):
-        if quantity >= product.quantity:
-            self.product = product
-            self.quantity = quantity
-        else:
-            print(f"Not enought items in warehouse. At this moment we have: {self.product.quantity} pieces")
-            return
+        self.product = product
+        self.quantity = quantity
 
     def calculate_total(self):
         return self.product.price * self.quantity
@@ -21,7 +17,25 @@ class Order:
         self.orderItems: list[OrderItem] = []
     
     def add_item(self, product, quantity):
-        pass
+        if quantity > product.quantity:
+            print(f"Not enought items in warehouse. At this moment we have: {product.quantity} pieces")
+        else:
+            order_item = OrderItem(product, quantity)
+            self.orderItems.append(order_item)
+            product.quantity -= quantity
+            print(f"Added {quantity} {product.name} to the order.")
+    
+    def calculate_total(self):
+        return sum(order.product.price * order.quantity for order in self.orderItems)
+    
+    def show_order_summary(self):
+        print("Total orders:") 
+        for order in self.orderItems:
+            print(f"In this order was ordered: {order.quantity} pieces of {order.product.name} in price:" +
+                 f" {order.product.price:.2f}$ per piece. Total cost: {order.calculate_total():.2f}$  ")
+          
+            #wyświetla podsumowanie zamówienia (produkty, ilości, cena za sztukę, całkowity koszt).
+
 
 # Tworzenie produktów
 apple = Product("Apple", 0.5, 100)
@@ -29,6 +43,8 @@ banana = Product("Banana", 0.3, 50)
 
 # Tworzenie zamówienia
 order = Order()
-"""order.add_item(apple, 10)
+order.add_item(apple, 10)
 order.add_item(banana, 60)  # Błąd, za dużo sztuk
-order.add_item(banana, 20)"""
+order.add_item(banana, 20)
+print(order.calculate_total())
+order.show_order_summary()
