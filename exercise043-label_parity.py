8f2aee3cf01a21c13d0bcb678c5765d7395601f4
Marcin <marcingieras1a@gmail.com>
2025-07-08 06:49:02 +0200

    exc044

---
+++ exercise043-label_parity.py
@@ -0,0 +1,8 @@
+# Napisz funkcję, która przyjmie listę liczb całkowitych i zwróci nową listę, 
+# w której każda liczba zostanie zastąpiona słowem - "even" jeśli parzysta, "odd" jeśli nieparzysta
+def label_parity(nums: list[int]) -> list[str]:
+    return ["even" if num % 2 == 0 else "odd" for num in nums]
+
+
+
+print(label_parity([1, 2, 3, 4, 5]))
---
+++ exercise044-count_letters.py
@@ -0,0 +1,11 @@
+# Napisz funkcję, która przyjmie string i zwróci słownik, w którym:
+# klucze to litery (małe i wielkie traktuj jako jedno),
+# wartości to liczba ich wystąpień w tekście (ignoruj spacje i znaki interpunkcyjne).
+def count_letters(text: str) -> dict[str, int]:
+    for char in text:
+        if char.lower():
+            char.
+            print(char)
+
+
+count_letters("Hello, world!")
