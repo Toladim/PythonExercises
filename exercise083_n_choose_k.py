6d9bb129c28a0827c659482a12999ea7357d6489
Marcin <marcingieras1a@gmail.com>
2025-07-19 21:01:09 +0200

    exc084

---
+++ exercise083_n_choose_k.py
@@ -0,0 +1,10 @@
+# Napisz funkcję, która oblicza wartość symbolu Newtona C(n, k) 
+# (tzw. n choose k = C(n, k)), wykorzystując funkcję silnia(n) z poprzedniej fazy.
+# Przykład wywołania: n_choose_k(5, 2) Wynik: 10 (ponieważ 5 choose 2 = 10)
+# C(n, k) = n! / (k! × (n − k)!)
+from exercise066_factorial_sum import factorial
+
+def n_choose_k(n: int, k: int) -> int:
+    return int((factorial(n) / (factorial(k) * factorial(n - k))))
+
+print(n_choose_k(5, 2))
---
+++ exercise084_fizz_buzz.py
@@ -0,0 +1,5 @@
+# Napisz funkcję, która wypisuje liczby od 1 do N, zastępując wielokrotności 3 słowem "Fizz",
+# wielokrotności 5 słowem "Buzz", a wielokrotności 3 i 5 jednocześnie słowem "FizzBuzz" 
+# (klasyczny problem FizzBuzz).
+# Przykład wywołania: fizz_buzz(15) 
+# Wynik: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
