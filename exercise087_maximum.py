6ca7a774ddda4c560b0942cc665d15921c08c6bd
Marcin <marcingieras1a@gmail.com>
2025-07-20 11:28:40 +0200

    exc087

---
+++ exercise087_maximum.py
@@ -0,0 +1,6 @@
+# Napisz funkcję, która znajduje maksymalny element w liście liczb.
+# Przykład wywołania: maximum([5, 1, 9, 3]) Wynik: 9
+def maximum(nums: list[int]) -> int:
+    return max(nums)
+
+print(maximum([5, 1, 9, 3]))
