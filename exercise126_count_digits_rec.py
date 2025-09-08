31d138c0dfbdac372739d815842ab781a1f55aaa
Marcin <marcingieras1a@gmail.com>
2025-09-08 19:05:26 +0200

    exc127

--- exercise126_count_digits_rec.py
+++ exercise126_count_digits_rec.py
@@ -1,2 +1,10 @@
 # Napisz funkcję, która zlicza liczbę cyfr w zadanej liczbie (rekurencyjnie).
-# Przykład wywołania: liczCyfryRek(5020) Wynik: 4
+# Przykład wywołania: count_digits_rec(5020) Wynik: 4
+
+def count_digits_rec(num: int) -> int:
+    if num < 10:
+        return 1
+    else:
+        return count_digits_rec(num // 10) + 1
+    
+print(count_digits_rec(5020))
